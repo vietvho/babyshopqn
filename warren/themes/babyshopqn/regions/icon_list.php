f9c20d6f73c56e64ba19a43bb491817161146cdc
viet nguyen <viet.nguyen@xenia.tech>
2021-12-28 22:19:07 +0700

    svg render

--- warren/themes/babyshopqn/regions/icon_list.php
+++ warren/themes/babyshopqn/regions/icon_list.php
@@ -7,7 +7,7 @@ if (!is_array($icon_list)) return;
     <div class="d_row">
         <?php foreach ($icon_list as $list_item):?>
             <div class="icon-list--item d_col">
-                <?php bbs_render_image($list_item['icon']['url'],["alt"=> $list_item['icon']['title'], "class"=>"list-icon"]);?>
+                <?php bbs_render_image($list_item['icon']['url'],["alt"=> $list_item['icon']['title'], "class"=>"list-icon svg"]);?>
                 <span class="list-text"><?= $list_item['content'];?></span>
             </div>
         <?php endforeach;?>
